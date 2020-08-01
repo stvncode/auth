63fa6adbc7dcb97ca0b0118280eaaef022f68b29
Yukio Senpai <steven.traver20@gmail.com>
2020-08-01 03:24:43 +0200

    check if token is valid and refresh if is not

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -2,7 +2,7 @@ import { User } from "./entity/User";
 import { sign } from "jsonwebtoken";
 
 export const createAccessToken = (user: User) => {
-    return sign({userId: user.id}, process.env.ACCESS_TOKEN_SECRET!, {expiresIn: '30min'})
+    return sign({userId: user.id}, process.env.ACCESS_TOKEN_SECRET!, {expiresIn: '15sec'})
 }
 export const createRefreshToken = (user: User) => {
     return sign({userId: user.id, tokenVersion: user.tokenVersion}, process.env.REFRESH_TOKEN_SECRET!, {expiresIn: '7d'})
--- web/src/pages/Bye.tsx
+++ web/src/pages/Bye.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { useByeQuery } from '../generated/graphql'
 
 export const Bye: React.FC = () => {
-    const {data, loading, error} = useByeQuery()
+    const {data, loading, error} = useByeQuery({
+        fetchPolicy: 'network-only'
+    })
 
     if(loading) {
         return <div>loading...</div>
